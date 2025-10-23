5f8554eb7ac602ff820869cba35f874680e0aa3b
BrunoEscada <brunoescada1990@gmail.com>
2025-10-23 13:46:16 +0100

    phase_1: fix names

--- App/etl/extract.py
+++ App/etl/extract.py
@@ -3,11 +3,11 @@ from pathlib import Path
 
 def extract_data():
     base_path = Path(__file__).parent.parent / "data" / "raw"
-    customers = pd.read_csv(base_path / "customers_info.csv")
-    products = pd.read_csv(base_path / "products_info.csv")
-    sales = pd.read_csv(base_path / "sales_raw.csv")
+    customers_df = pd.read_csv(base_path / "customers_info.csv")
+    products_df = pd.read_csv(base_path / "products_info.csv")
+    sales_df = pd.read_csv(base_path / "sales_raw.csv")
 
-    return customers, products, sales
+    return customers_df, products_df, sales_df
 
 if __name__ == "__main__":
     customers, products, sales = extract_data()
